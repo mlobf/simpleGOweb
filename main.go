8edb955b7d79f560e9e7e4f14529fa4f1815bc12
mlobf <marcoslemeborbafilho@gmail.com>
2022-04-28 21:00:25 -0300

    Add html  page

--- main.go
+++ main.go
@@ -6,16 +6,16 @@ import (
 	"net/http"
 )
 
+// Display the main message
 func homePage(w http.ResponseWriter, r *http.Request) {
+	html := "<strong>Hello, world</strong>"
+	w.Header().Set("Content-Type", "text/html")
 
-	fmt.Fprint(w, "Hello, world Vai Curintia!!")
-
+	fmt.Fprint(w, html)
 }
 
 func main() {
 	http.HandleFunc("/", homePage)
-
 	log.Println("Starting web server at port 8080")
 	http.ListenAndServe(":8080", nil)
-
 }
